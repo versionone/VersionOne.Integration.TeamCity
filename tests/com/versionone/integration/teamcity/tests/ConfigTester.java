a8848d421b4c53a3c02694727ff87653edbfdafa
Denis <Denis@542498e7-56f8-f544-a0f2-a85913ac812a>
2009-04-05 11:33:12 +0000

    tests added
    git-svn-id: https://svn.corp.versionone.net/svn/1/Trunk/V1Integration/Java/TeamCity@23691 542498e7-56f8-f544-a0f2-a85913ac812a

--- tests/com/versionone/integration/teamcity/tests/ConfigTester.java
+++ tests/com/versionone/integration/teamcity/tests/ConfigTester.java
@@ -1,49 +1,63 @@
 /*(c) Copyright 2008, VersionOne, Inc. All rights reserved. (c)*/
 package com.versionone.integration.teamcity.tests;
 
-import com.versionone.integration.common.IConfig;
 import com.versionone.integration.teamcity.Config;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.io.File;
+import java.util.regex.Pattern;
+
 
 public class ConfigTester {
+    private static final String DIR = ".";
 
-    @Test
-    public void testGetV1Instance() {
-        final Config settings = new Config("http://localhost", "1", "2", null, "field");
-
-        try {
-            settings.getV1Instance();
-            Assert.fail("This test has to call exception");
-        } catch (RuntimeException e) {
-            
-        }
+    @After
+    public void deleteCfgFile() {
+        new File(DIR, Config.CONFIG_FILENAME).delete();
     }
 
     @Test
-    public void testIsConnectionValidIncorrectUrl() {
-        final Config settings = new Config("http://incorrecturl", "1", "2", null, "field");
+    public void testDefaults() {
+        final Config cfg = new Config(DIR);
 
-        Assert.assertFalse(settings.isConnectionValid());
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testIsConnectionUrlIsNull() {
-        final IConfig settings = new Config(null, "1", "2", null, "field");
-    }
-
-    public void testIsConnectionUrlIsEmpty() {
-        final Config settings = new Config("", "1", "2", null, "field");
-        Assert.assertFalse(settings.isConnectionValid());
+        Assert.assertEquals("http://localhost/VersionOne/", cfg.getUrl());
+        Assert.assertEquals("admin", cfg.getUserName());
+        Assert.assertEquals("admin", cfg.getPassword());
+        Assert.assertEquals("Number", cfg.getReferenceField());
+        Assert.assertEquals("[A-Z]{1,2}-[0-9]+", cfg.getPatternObj().pattern());
     }
 
     @Test
-    public void testLoginIsEmpty() {
-        final IConfig settings = new Config("http://url", "", "2", null, "field");
-
-        Assert.assertNull(settings.getUserName());
+    public void testConnectionValid() {
+        final Config cfg = new Config(DIR);
+        cfg.setUrl("http://v1.co/V1");
+        Assert.assertFalse(cfg.isConnectionValid());
     }
 
+    @Test
+    public void testFileReadWrite() {
+        final String url = "http://v1.com/V1";
+        final String userName = "usr";
+        final String pass = "psw";
+        final Pattern pattern = Pattern.compile("ptr");
+        final String ref = "r";
+
+        final Config cfg = new Config(DIR);
+        cfg.setUrl(url);
+        cfg.setUserName(userName);
+        cfg.setPassword(pass);
+        cfg.setPattern(pattern);
+        cfg.setReferenceField(ref);
+        cfg.save();
+
+        final Config cfg2 = new Config(DIR);
+        Assert.assertEquals(url, cfg2.getUrl());
+        Assert.assertEquals(userName, cfg2.getUserName());
+        Assert.assertEquals(pass, cfg2.getPassword());
+        Assert.assertEquals(ref, cfg2.getReferenceField());
+        Assert.assertEquals(pattern.pattern(), cfg2.getPatternObj().pattern());
+    }
 }
